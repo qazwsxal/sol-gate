11ccb28c0a0096b1e8575bd6c0a0f5f23c839a02
Adam Leach <qazwsxalan@gmail.com>
2022-09-24 18:34:58 +0100

    refactor into get_vp_path method for reuse.

--- vp/src/fs.rs
+++ vp/src/fs.rs
@@ -33,25 +33,11 @@ pub struct ReadDir {
 }
 
 pub fn read_dir<P: Into<PathBuf>>(path: P) -> io::Result<ReadDir> {
-    let path = path.into();
-    let mut vp_filepath: PathBuf = path.clone();
-    let mut folders: Vec<String> = Vec::new();
-    while vp_filepath.is_file() == false {
-        folders.push(
-            vp_filepath
-                .file_name()
-                .ok_or(io::Error::from(io::ErrorKind::NotFound))?
-                .to_os_string()
-                .into_string()
-                .map_err(|_| io::Error::from(io::ErrorKind::NotFound))?,
-        );
-        vp_filepath = vp_filepath
-            .parent()
-            .ok_or(io::Error::from(io::ErrorKind::NotFound))?.to_path_buf();
-    }
+    let path: PathBuf = path.into();
+    let (vp_filepath, folders) = get_vp_path(&path)?;
+
 
     let mut vpcontents: Vec<VPEntry> = index(&mut File::open(&vp_filepath)?)?.contents;
-    folders.reverse();
     for folder in folders.into_iter() {
         vpcontents = vpcontents
             .into_iter()
@@ -68,3 +54,58 @@ pub fn read_dir<P: Into<PathBuf>>(path: P) -> io::Result<ReadDir> {
         vpcontents,
     })
 }
+
+pub fn read_entry<P: Into<PathBuf>>(path: P) -> io::Result<VPEntry> {
+    let path: PathBuf = path.into();
+    let (vp_filepath, mut folders) = get_vp_path(&path)?;
+
+    let mut vpcontents: Vec<VPEntry> = index(&mut File::open(&vp_filepath)?)?.contents;
+    let file = folders.pop().ok_or(io::Error::from(io::ErrorKind::NotFound))?;
+    for folder in folders.into_iter() {
+        vpcontents = vpcontents
+            .into_iter()
+            .filter_map(|x| match x {
+                VPEntry::Dir(d) => Some(d),
+                _ => None,
+            })
+            .find(|x| x.name == folder)
+            .ok_or(io::Error::from(io::ErrorKind::NotFound))?
+            .contents
+    }
+    vpcontents
+    .into_iter()
+    .filter_map(|x| match x {
+        VPEntry::File(f) => Some(f),
+        _ => None,
+    })
+    .find(|x| x.name == file)
+    .map(|x| VPEntry::File(x))
+    .ok_or(io::Error::from(io::ErrorKind::NotFound))
+
+}
+
+
+pub fn read_file<T: Into<VPEntry>>(entry: T) -> Vec<u8> {
+    todo!()
+}
+
+fn get_vp_path(path: &PathBuf) -> Result<(PathBuf, Vec<String>), std::io::Error> {
+    let mut vp_filepath: PathBuf = path.clone();
+    let mut folders: Vec<String> = Vec::new();
+    while vp_filepath.is_file() == false {
+        folders.push(
+            vp_filepath
+                .file_name()
+                .ok_or(io::Error::from(io::ErrorKind::NotFound))?
+                .to_os_string()
+                .into_string()
+                .map_err(|_| io::Error::from(io::ErrorKind::NotFound))?,
+        );
+        vp_filepath = vp_filepath
+            .parent()
+            .ok_or(io::Error::from(io::ErrorKind::NotFound))?.to_path_buf();
+    }
+    folders.reverse();
+    Ok((vp_filepath, folders))
+}
+
--- vp/src/lib.rs
+++ vp/src/lib.rs
@@ -7,6 +7,10 @@ pub mod types;
 
 #[cfg(test)]
 mod tests {
+    use std::fs::read;
+
+    use crate::types::VPEntry;
+
     use super::*;
 
     #[test]
@@ -18,9 +22,22 @@ mod tests {
     }
 
     #[test]
-    fn list() {
+    fn ls() {
         let vp_dir = fs::read_dir("./test_files/mv_radaricons.vp/data/").unwrap();
         println!("{:?}", vp_dir);
+    }
+    #[test]
+    fn read_entry() {
+        let entry: VPEntry = fs::read_entry("./test_files/mv_radaricons.vp/data/hud/radar-asteroid.dds").unwrap();
+        dbg!(entry);
+    }
+
+    #[test]
+    fn read_file() {
+        let mut raw_file = read("./test_files/radar-asteroid.dds").unwrap();
+
+        let mut vp_entry = fs::read_entry("./test_files/mv_radaricons.vp/data/hud/radar-asteroid.dds").unwrap();
+        let vp_file = fs::read_file(vp_entry);
     }
 
 }
