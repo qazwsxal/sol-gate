6115c6effef06c8e3762a333979dcba18288bdf1
Adam Leach <qazwsxalan@gmail.com>
2023-03-19 12:58:18 +0000

    turns out that config was needed to actually forward api requests

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,5 +2,18 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+
+	server: {
+		proxy: {
+			'/api': {
+				target: 'http://127.0.0.1:4000',
+				changeOrigin: true,
+			  },
+		},
+		fs: {
+			// Allow serving files from one level up to the project root
+			allow: [".."],
+		  },
+	}
 });
